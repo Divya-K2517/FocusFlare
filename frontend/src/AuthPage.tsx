7565d21c929ae97a59a657eba4b3972c8e20e8bc
Divya-K2517 <115424525+Divya-K2517@users.noreply.github.com>
2025-07-06 00:00:47 -0400

    AuthPath UI changes

--- frontend/src/AuthPage.tsx
+++ frontend/src/AuthPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import {LoginPage} from "./LoginPage";
 import {SignupPage} from "./SignupPage";
-//page for login/signup
+//page for login & signup
 const AuthPage: React.FC = () => {
   const [showSignup, setShowSignup] = useState(false);
 
@@ -9,7 +9,7 @@ const AuthPage: React.FC = () => {
     <div className="loginSignupForm">
     <div style={{ maxWidth: 400, margin: "auto", padding: "2rem" }}>
       {showSignup ? <SignupPage /> : <LoginPage />}
-      <button onClick={() => setShowSignup(!showSignup)} style={{ marginTop: 20 }}>
+      <button onClick={() => setShowSignup(!showSignup)} style={{ marginTop: 20, background: 'none', color: '#4A323B', border: 'none', textDecoration: 'underline', textUnderlineOffset: '1px',cursor: 'pointer'}}>
         {showSignup ? "Already have an account? Log in" : "Don't have an account? Sign up"}
       </button>
     </div>
